78ac26a15d068c2b88c9fb0f49539cf124104900
John Adrian Biticon <john.adrian.bacon@gmail.com>
2025-10-03 02:36:34 +0000

    update from server

--- resources/js/actions/App/Http/Controllers/Api/DashboardApiController.ts
+++ resources/js/actions/App/Http/Controllers/Api/DashboardApiController.ts
@@ -1,9 +1,9 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
 export const getStats = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: getStats.url(options),
     method: 'get',
@@ -16,72 +16,75 @@ getStats.definition = {
 
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
 getStats.url = (options?: RouteQueryOptions) => {
     return getStats.definition.url + queryParams(options)
 }
 
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
 getStats.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: getStats.url(options),
     method: 'get',
 })
+
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
 getStats.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: getStats.url(options),
     method: 'head',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
-    const getStatsForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-        action: getStats.url(options),
-        method: 'get',
-    })
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
+const getStatsForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getStats.url(options),
+    method: 'get',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
-        getStatsForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: getStats.url(options),
-            method: 'get',
-        })
-            /**
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
+getStatsForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getStats.url(options),
+    method: 'get',
+})
+
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getStats
- * @see app/Http/Controllers/Api/DashboardApiController.php:19
- * @route '/api/dashboard/stats'
- */
-        getStatsForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: getStats.url({
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'HEAD',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'get',
-        })
-    
-    getStats.form = getStatsForm
+* @see app/Http/Controllers/Api/DashboardApiController.php:19
+* @route '/api/dashboard/stats'
+*/
+getStatsForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getStats.url({
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+getStats.form = getStatsForm
+
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
 export const getInstructorProfile = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: getInstructorProfile.url(options),
     method: 'get',
@@ -94,67 +97,70 @@ getInstructorProfile.definition = {
 
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
 getInstructorProfile.url = (options?: RouteQueryOptions) => {
     return getInstructorProfile.definition.url + queryParams(options)
 }
 
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
 getInstructorProfile.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: getInstructorProfile.url(options),
     method: 'get',
 })
+
 /**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
 getInstructorProfile.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: getInstructorProfile.url(options),
     method: 'head',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
-    const getInstructorProfileForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-        action: getInstructorProfile.url(options),
-        method: 'get',
-    })
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
+const getInstructorProfileForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getInstructorProfile.url(options),
+    method: 'get',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
-        getInstructorProfileForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: getInstructorProfile.url(options),
-            method: 'get',
-        })
-            /**
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
+getInstructorProfileForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getInstructorProfile.url(options),
+    method: 'get',
+})
+
+/**
 * @see \App\Http\Controllers\Api\DashboardApiController::getInstructorProfile
- * @see app/Http/Controllers/Api/DashboardApiController.php:58
- * @route '/api/instructor/profile'
- */
-        getInstructorProfileForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: getInstructorProfile.url({
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'HEAD',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'get',
-        })
-    
-    getInstructorProfile.form = getInstructorProfileForm
+* @see app/Http/Controllers/Api/DashboardApiController.php:58
+* @route '/api/instructor/profile'
+*/
+getInstructorProfileForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: getInstructorProfile.url({
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+getInstructorProfile.form = getInstructorProfileForm
+
 const DashboardApiController = { getStats, getInstructorProfile }
 
 export default DashboardApiController
